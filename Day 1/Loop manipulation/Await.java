587067d1acbd5ff32d7c569d169c8afa643aec68
RIOT <71219830+KrypDeveloper@users.noreply.github.com>
2025-01-31 18:21:04 -0300

    Create Await.java
    does nothing with a loop until a condition(loop) be false

---
+++ Day 1/Loop manipulation/Await.java
@@ -0,0 +1,15 @@
+// my first script ever :D
+
+public class await {
+    public static void main(String[] args) {
+        boolean loop = true;
+
+        do {
+            await();
+        } while (loop == true);
+    };
+
+    public static void await() {
+        return;
+    }
+}
